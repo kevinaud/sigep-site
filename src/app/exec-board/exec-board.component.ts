cb126fbcb34df6a4b2a41245e80ac77785d3aa44
Kevin Aud <kevinaud@gmail.com>
2016-10-25 17:33:51 -0500

    added theme and basic routes

---
+++ src/app/exec-board/exec-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exec-board',
+  templateUrl: './exec-board.component.html',
+  styleUrls: ['./exec-board.component.css']
+})
+export class ExecBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
